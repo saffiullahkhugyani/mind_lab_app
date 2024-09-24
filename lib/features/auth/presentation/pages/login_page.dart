0c1ae307534085fef012ae3e13a139640495b958
saffiullahkhugyani <saffiullah.khugyani@gmail.com>
2024-09-24 16:00:28 +0500

    un commented code for social login

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -219,63 +219,63 @@ class _LoginPageState extends State<LoginPage> {
                       ),
 
                       // or continue with
-                      // Padding(
-                      //   padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                      //   child: Row(
-                      //     children: [
-                      //       Expanded(
-                      //         child: Divider(
-                      //           thickness: 0.5,
-                      //           color: Colors.grey[400],
-                      //         ),
-                      //       ),
-                      //       Padding(
-                      //         padding:
-                      //             const EdgeInsets.symmetric(horizontal: 10.0),
-                      //         child: Text(
-                      //           'Or continue with',
-                      //           style: TextStyle(color: Colors.grey[700]),
-                      //         ),
-                      //       ),
-                      //       Expanded(
-                      //         child: Divider(
-                      //           thickness: 0.5,
-                      //           color: Colors.grey[400],
-                      //         ),
-                      //       ),
-                      //     ],
-                      //   ),
-                      // ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Divider(
+                                thickness: 0.5,
+                                color: Colors.grey[400],
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10.0),
+                              child: Text(
+                                'Or continue with',
+                                style: TextStyle(color: Colors.grey[700]),
+                              ),
+                            ),
+                            Expanded(
+                              child: Divider(
+                                thickness: 0.5,
+                                color: Colors.grey[400],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
 
-                      // const SizedBox(
-                      //   height: 25,
-                      // ),
-                      // Row(
-                      //   mainAxisAlignment: MainAxisAlignment.center,
-                      //   children: [
-                      //     SocialLoginButton(
-                      //         onTap: () {
-                      //           context
-                      //               .read<AuthBloc>()
-                      //               .add(AuthLoginWithGoogle());
-                      //         },
-                      //         imagePath: 'lib/assets/social_logos/google.png'),
-                      //     if (Platform.isIOS) ...[
-                      //       const SizedBox(
-                      //         width: 25,
-                      //       ),
-                      //       SocialLoginButton(
-                      //           onTap: () {
-                      //             showFlashBar(
-                      //               context,
-                      //               "Feature Coming Soon",
-                      //               FlashBarAction.info,
-                      //             );
-                      //           },
-                      //           imagePath: 'lib/assets/social_logos/apple.png'),
-                      //     ]
-                      //   ],
-                      // ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SocialLoginButton(
+                              onTap: () {
+                                context
+                                    .read<AuthBloc>()
+                                    .add(AuthLoginWithGoogle());
+                              },
+                              imagePath: 'lib/assets/social_logos/google.png'),
+                          if (Platform.isIOS) ...[
+                            const SizedBox(
+                              width: 25,
+                            ),
+                            SocialLoginButton(
+                                onTap: () {
+                                  showFlashBar(
+                                    context,
+                                    "Feature Coming Soon",
+                                    FlashBarAction.info,
+                                  );
+                                },
+                                imagePath: 'lib/assets/social_logos/apple.png'),
+                          ]
+                        ],
+                      ),
                     ],
                   ),
                 );
