ea163a2900510f0c090fabc0aba66d0e6f8a6df1
saffiullahkhugyani <saffiullah.khugyani@gmail.com>
2024-10-14 16:47:15 +0500

    functionality for button pressed changed

--- lib/features/arcade_game_two/presentation/widgets/arrow_button.dart
+++ lib/features/arcade_game_two/presentation/widgets/arrow_button.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class ArrowButton extends StatelessWidget {
+class ArrowButton extends StatefulWidget {
   final IconData arrowIcon;
   final Function() onTap;
   const ArrowButton({
@@ -8,6 +10,36 @@ class ArrowButton extends StatelessWidget {
     required this.arrowIcon,
     required this.onTap,
   });
+
+  @override
+  State<ArrowButton> createState() => _ArrowButtonState();
+}
+
+class _ArrowButtonState extends State<ArrowButton> {
+  //how often method will be called
+  Duration delay = const Duration(milliseconds: 10);
+
+  // decalring timer
+  Timer? timer;
+
+  void continuousWork() {
+    debugPrint("I am working c ${timer!.tick}");
+  }
+
+  void onPressEnd() {
+    debugPrint("Job END");
+    timer?.cancel();
+    timer = null;
+  }
+
+  void onPressStart() {
+    if (timer != null) return;
+    debugPrint("Job started");
+    timer = Timer.periodic(delay, (timer) {
+      // continuousWork();
+      widget.onTap();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,20 +50,29 @@ class ArrowButton extends StatelessWidget {
         borderRadius: BorderRadius.circular(8),
         child: InkWell(
           borderRadius: BorderRadius.circular(8),
-          onTap: () {
-            onTap();
-          },
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            width: 100,
-            height: 100,
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.white),
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.grey.withOpacity(0.2)),
-            child: Icon(
-              arrowIcon,
-              size: 50,
+          onTap: () {},
+          child: GestureDetector(
+            onTapDown: (v) {
+              onPressStart();
+            },
+            onTapUp: (v) {
+              onPressEnd();
+            },
+            onTap: () {
+              // onTap();
+            },
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.grey.withOpacity(0.2)),
+              child: Icon(
+                widget.arrowIcon,
+                size: 50,
+              ),
             ),
           ),
         ),
